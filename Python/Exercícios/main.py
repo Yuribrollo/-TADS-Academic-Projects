46e29a10c92c0efb66818a0f07d924e8c18a9f96
Yuribrollo <69725718+Yuribrollo@users.noreply.github.com>
2026-07-08 16:09:10 -0300

    Delete Python/Exercícios directory

--- Python/Exercícios/main.py
+++
@@ -1,9 +0,0 @@
-nome_vendedor = input("Nome do vendedor: ")
-salario_fixo = float(input("Salário fixo: "))
-montante = float(input("Montante de vendas: "))
-
-comissao = montante * 0.15
-
-valor_a_receber = salario_fixo + comissao
-
-print(f"O vendedor {nome_vendedor} receberá: R$ %.2f" %valor_a_receber)
